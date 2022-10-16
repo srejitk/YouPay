3fb997f1b9635cf991ead791445ad775ac56b8a8
Sreejith K <srejitk@gmail.com>
2022-10-17 02:21:24 +0530

    Feat : Added Validations, Redirects

--- youpay/src/services/index.js
+++ youpay/src/services/index.js
@@ -1,37 +1,80 @@
 import axios from 'axios';
+import { toast } from 'react-hot-toast';
 
 const API = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL,
 });
 
 API.interceptors.request.use((req) => {
-  if (localStorage.getItem('Token')) {
-    req.headers.Authorization = `Bearer ${JSON.parse(
-      localStorage.getItem('Token')
-    )}`;
+  if (localStorage.getItem('USER_TOKEN')) {
+    console.log(typeof localStorage.getItem('USER_TOKEN'));
+    req.headers.Authorization = `Bearer ${localStorage.getItem('USER_TOKEN')}`;
   }
   return req;
 });
 
-export const signUpUser = async (formData) =>
-  await API.post(`/auth/signup`, formData);
-
-export const signInUser = async (formData) => {
+export const signUpUser = async (formData, dispatch) => {
   try {
-    const res = await API.post(`/auth/signin`, formData);
-    const { token } = res;
-    localStorage.setItem('Token', token);
+    const res = await API.post(`/auth/signup`, formData);
+    const {
+      status,
+      data: { newUser, token },
+    } = res;
+
+    if (status === 201) {
+      dispatch({ type: 'AUTH', payload: token });
+      dispatch({ type: 'SET_USER', payload: newUser });
+    }
   } catch (error) {
     console.log(error);
   }
 };
 
-export const sendBorrowReq = async (borrowReqData) => {
-  // TODO Add rest of the validations + Toast
-  // TODO Add Navigation to Home post request
+export const signInUser = async (
+  formData,
+  dispatch,
+  navigate,
+  pageToRedirect
+) => {
+  const toastId = toast.loading('Signing you in...');
   try {
-    await API.post('/borrow', borrowReqData);
+    const res = await API.post(`/auth/signin`, formData);
+    const {
+      status,
+      data: { existingUser, token },
+    } = res;
+
+    if (status === 200) {
+      dispatch({ type: 'AUTH', payload: token });
+      dispatch({ type: 'SET_USER', payload: existingUser });
+      navigate(pageToRedirect);
+      toast.success('Welcome back', { id: toastId });
+    }
   } catch (error) {
     console.log(error);
+    toast.error("Couldn't sign you in", { id: toastId });
+  }
+};
+
+export const logout = (dispatch, navigate) => {
+  dispatch({ type: 'LOGOUT' });
+  navigate('/auth');
+};
+export const sendBorrowReq = async (borrowReqData, dispatch) => {
+  const toastId = toast.loading('Sending request...');
+  try {
+    const res = await API.post('/borrow', borrowReqData);
+    const { status, data } = res;
+    if (status === 201) {
+      dispatch({ type: 'SET_USER', payload: data });
+      toast.success('Request sent!', {
+        id: toastId,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    toast.error("Couldn't send your request", {
+      id: toastId,
+    });
   }
 };
