3b0b36e8051a6f5b4ce7890a8a05cc4e1b2b6615
Sreejith K <srejitk@gmail.com>
2022-10-17 03:26:48 +0530

    Feat : Added Logout

--- youpay/src/components/Header/Header.jsx
+++ youpay/src/components/Header/Header.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useUser } from '../../contexts/user/UserContext';
+import { logout } from '../../services';
 
 export const Header = () => {
+  const { dispatch } = useUser();
+  const navigate = useNavigate();
+  const isLoggedin = !!localStorage.getItem('USER_TOKEN');
   return (
-    <header className="sticky top-0 flex h-20 max-w-[100vw] items-center justify-center gap-10 border-2 border-gray-700">
-      <Link to={'/'}>Home</Link>
-      <Link to={'/auth'}>Auth</Link>
-      <Link to={'/borrow'}>Borrow</Link>
+    <header className="sticky top-0 z-30 flex h-20 max-w-[100vw] items-center justify-between gap-10 bg-white px-10 shadow-md lg:px-20">
+      <h1 className="text-xl font-medium">
+        <span className="text-blue-500">You</span>Pay
+      </h1>
+      <div className="flex gap-10">
+        <Link to={'/'}>Home</Link>
+        <Link to={'/auth'}>Auth</Link>
+        <Link to={'/borrow'}>Borrow</Link>
+      </div>
+      <button
+        className={`flex items-center ${
+          isLoggedin ? 'visible' : 'invisible '
+        } justify-center rounded-full border-2 border-gray-300 px-3 py-1`}
+        onClick={() => logout(dispatch, navigate)}
+      >
+        Logout
+      </button>
     </header>
   );
 };
--- youpay/src/contexts/user/reducer.js
+++ youpay/src/contexts/user/reducer.js
@@ -7,6 +7,9 @@ export const userReducer = (state, { type, payload }) => {
       return { ...state, userData: payload };
     case 'AUTHENTICATE_USER':
       return { ...state, isUserAuthenticated: payload };
+    case 'LOGOUT':
+      localStorage.removeItem('USER_TOKEN');
+      return { ...state, userData: {} };
     default:
       break;
   }
