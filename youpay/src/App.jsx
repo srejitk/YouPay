e9690028303e29ef3fc969a8c2cc4ee60ec82ccd
Sreejith K <srejitk@gmail.com>
2022-10-16 16:33:08 +0530

    Feat : Added react-hot-toasts

--- youpay/src/App.jsx
+++ youpay/src/App.jsx
@@ -2,12 +2,14 @@ import './App.css';
 import React from 'react';
 import { RouteConfig } from './config/RouteConfig';
 import { Header } from './components';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <div className="App relative">
       <Header />
       <RouteConfig />
+      <Toaster />
     </div>
   );
 }
