5ca62122fe54a1f8cfc581165a189aeff27a1f9c
Sreejith K <srejitk@gmail.com>
2022-10-17 03:28:53 +0530

    Feat : Added Borrow Page

--- youpay/src/routes/Borrow.page.jsx
+++ youpay/src/routes/Borrow.page.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import { BorrowForm } from '../components/BorrowForm/BorrowForm';
 
 export const Borrow = () => {
-  return <div>Borrow.page</div>;
+  return (
+    <div className="background-grad flex items-center justify-center">
+      <BorrowForm />
+    </div>
+  );
 };
