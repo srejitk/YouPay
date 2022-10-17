421ac57d9979934ff498ce7f8f9703b522127f11
Sreejith K <srejitk@gmail.com>
2022-10-17 15:45:07 +0530

    Fix : invalid paths

--- youpay/src/config/RouteConfig.jsx
+++ youpay/src/config/RouteConfig.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import { Auth } from '../routes/Auth.page';
 import { Borrow } from '../routes/Borrow.page';
 import { Home } from '../routes/Home.Page';
+
 import RequireAuth from '../utils/RequiresAuth';
 export const RouteConfig = () => {
   return (
