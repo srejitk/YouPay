6dbfc6068c67b8e686eb24c4f1905861e57b3ec3
Sreejith K <srejitk@gmail.com>
2022-10-17 15:20:55 +0530

    Feat : Added Transaction list

--- youpay/src/components/Cards/Overview/Overview.jsx
+++ youpay/src/components/Cards/Overview/Overview.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment';
 
 export const Overview = ({ borrowedFrom, lendedTo }) => {
   const totalBorrowed =
@@ -21,6 +22,28 @@ export const Overview = ({ borrowedFrom, lendedTo }) => {
           <h2 className="mt-2 text-gray-500">Money Lended</h2>
           <p className="text-xl">&#8377;{totalLended}</p>
         </div>
+      </div>
+      <div className="my-2 h-[1px] w-full bg-gray-50/20"></div>
+      <div>
+        <h1 className="my-3 text-xl">Lended to</h1>
+        {lendedTo?.length > 0 ? (
+          lendedTo?.map((item) => (
+            <div
+              key={item._id}
+              className="flex w-1/2 items-center justify-between"
+            >
+              <div>
+                <h2 className="text-lg font-light text-gray-500">{item.upi}</h2>
+                <p>{moment(item.lendedDate).format(' Mo MMM YYYY')}</p>
+              </div>
+              <p className="text-xl">&#8377;{item.amount}</p>
+            </div>
+          ))
+        ) : (
+          <p className="mt-2 text-gray-500">
+            You haven&#39;t lended to anyone yet
+          </p>
+        )}
       </div>
     </div>
   );
